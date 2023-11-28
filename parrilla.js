4db59b411312008b143d8fa794500963e9b4f401
simonisdev <sagredo83@gmail.com>
2023-11-28 15:33:15 -0300

    maquetacion pagina de capitulo

--- parrilla.js
+++ parrilla.js
@@ -1,82 +1,82 @@
 const capitulos = [
     {
       id: 'cap1',
-      imagen: '/images/cap1_01.jpg',
+      imagen: '/images/miniaturas/cap1_01.png',
       episodio: '1',
       titulo: 'Ciudad Bohemia',
-      sinopsis: 'Ciudad Bohemia',
+      sinopsis: 'Sinopsis: Ciudad Bohemia',
       youtubeID: 'ffEFNTG-PlI'
     },
     {
       id: 'cap2',
-      imagen: '/images/cap2_01.jpg',
+      imagen: '/images/miniaturas/cap2_01.png',
       episodio: '2',
       titulo: 'Popayán Construida por Mujeres',
-      sinopsis: 'Popayán Construida por Mujeres',
+      sinopsis: 'Sinopsis: Popayán Construida por Mujeres',
       youtubeID: 'codigoVideo'
     },
     {
       id: 'cap3',
-      imagen: '/images/cap3_01.jpg',
+      imagen: '/images/miniaturas/cap3_01.png',
       episodio: '3',
       titulo: 'Popayán Capital de las Culturas',
-      sinopsis: 'Popayán Capital de las Culturas',
+      sinopsis: 'Sinopsis: Popayán Capital de las Culturas',
       youtubeID: 'codigoVideo'
     },
     {
       id: 'cap4',
-      imagen: '/images/cap4_01.jpg',
+      imagen: '/images/miniaturas/cap4_01.png',
       episodio: '4',
       titulo: 'Gastronomía en Popayán',
-      sinopsis: 'Gastronomía en Popayán',
+      sinopsis: 'Sinopsis: Gastronomía en Popayán',
       youtubeID: 'codigoVideo'
     },
     {
       id: 'cap5',
-      imagen: '/images/cap5_01.jpg',
+      imagen: '/images/miniaturas/cap5_01.png',
       episodio: '5',
       titulo: 'El Lenguaje de las Flores',
-      sinopsis: 'El Lenguaje de las Flores',
+      sinopsis: 'Sinopsis: El Lenguaje de las Flores',
       youtubeID: 'codigoVideo'
     },
     {
       id: 'cap6',
-      imagen: '/images/cap6_01.jpg',
+      imagen: '/images/miniaturas/cap6_01.png',
       episodio: '6',
       titulo: 'Ser Campesino por un Día',
-      sinopsis: 'Ser Campesino por un Día',
+      sinopsis: 'Sinopsis: Ser Campesino por un Día',
       youtubeID: 'codigoVideo'
     },
     {
       id: 'cap7',
-      imagen: '/images/cap7_01.jpg',
+      imagen: '/images/miniaturas/cap7_01.png',
       episodio: '7',
       titulo: 'Popayán de Aventura',
-      sinopsis: 'Popayán de Aventura',
+      sinopsis: 'Sinopsis: Popayán de Aventura',
       youtubeID: 'codigoVideo'
     },
     {
       id: 'cap8',
-      imagen: '/images/cap8_01.jpg',
+      imagen: '/images/miniaturas/cap8_01.png',
       episodio: '8',
       titulo: 'Un Viaje a las Estrellas',
-      sinopsis: 'Un Viaje a las Estrellas',
+      sinopsis: 'Sinopsis: Un Viaje a las Estrellas',
       youtubeID: 'codigoVideo'
     },
     {
       id: 'cap9',
-      imagen: '/images/cap9_01.jpg',
+      imagen: '/images/miniaturas/cap9_01.png',
       episodio: '9',
       titulo: 'Popayán Ciudad de Pasión',
-      sinopsis: 'Popayán Ciudad de Pasión',
+      sinopsis: 'Sinopsis: Popayán Ciudad de Pasión',
       youtubeID: 'codigoVideo'
     },
     {
       id: 'cap10',
-      imagen: '/images/cap10_01.jpg',
+      imagen: '/images/miniaturas/cap10_01.png',
       episodio: '10',
       titulo: 'Popayán Ciudad del Amor Eterno',
-      sinopsis: 'Popayán Ciudad del Amor Eterno',
+      sinopsis: 'Sinopsis: Popayán Ciudad del Amor Eterno',
       youtubeID: 'codigoVideo'
     }  
 ];
@@ -106,16 +106,25 @@ function generarMiniaturas() {
     imgCapitulo.src = capitulo.imagen;
     imgCapitulo.alt = capitulo.titulo;
 
-    const divSinopsis = document.createElement('div');
-    divSinopsis.classList.add('sinopsis');
-    divSinopsis.textContent = capitulo.sinopsis;
+    // const divSinopsis = document.createElement('div');
+    // divSinopsis.classList.add('sinopsis');
+    // divSinopsis.textContent = capitulo.sinopsis;
 
-    const enlaceCapitulo = document.createElement('a');
-    enlaceCapitulo.href = `https://www.youtube.com/watch?v=${capitulo.youtubeID}`;
-    enlaceCapitulo.target = '_blank'; // Abre el enlace en una nueva pestaña
+    const divTitulo = document.createElement('div');
+    divTitulo.classList.add('titulo');
+    divTitulo.textContent = `Capitulo `+capitulo.episodio+` - `+capitulo.titulo;
+
+    divCapitulo.addEventListener('click', function () {
+      window.location.href = '/capitulo' + capitulo.episodio + '.html';
+      });
+
+    // const enlaceCapitulo = document.createElement('a');
+    // enlaceCapitulo.href = `https://www.youtube.com/watch?v=${capitulo.youtubeID}`;
+    // enlaceCapitulo.target = '_blank'; // Abre el enlace en una nueva pestaña
 
     divCapitulo.appendChild(imgCapitulo);
-    divCapitulo.appendChild(divSinopsis);
+    // divCapitulo.appendChild(divSinopsis);
+    divCapitulo.appendChild(divTitulo);
 
     seccionCapitulos.appendChild(divCapitulo);
   });
